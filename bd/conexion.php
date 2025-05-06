0864944f3784cc28c4fe6a64a6a06b906f67c8ae
AlexDrigetty <pieroalexander.codeh@gmail.com>
2025-05-06 16:10:24 -0500

    comienzo de la version_02

---
+++ bd/conexion.php
@@ -0,0 +1,15 @@
+<?php
+$local = "localhost:3308";
+$username = "root";
+$password = "";
+$base_datos = "noticias_NI";
+
+$conexion = mysqli_connect($local, $username, $password, $base_datos);
+
+if(!$conexion){
+    echo "OCURRIO UN ERROR AL CONECTAR LA BASE DE DATOS";
+} else {
+    echo "CONEXION ESTABLECIDA";
+}
+
+?>
